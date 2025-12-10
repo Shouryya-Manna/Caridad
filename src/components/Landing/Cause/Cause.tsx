6e949331a7f61b0b84c7f12cd821bd9e0b78a368
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-10 19:05:52 +0530

    Pushed 1st phase of file structure

---
+++ src/components/Landing/Cause/Cause.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cause() {
+  return <div>Cause</div>;
+}
---
+++ src/components/Landing/LatestStories/LatestStories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LatestStories() {
+  return <div>LatestStories</div>;
+}
