fc42235ecedd387690ffa6dc0c39cdbea03da532
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-12 22:18:40 +0530

    Header Data Generated Via Chatgpt

--- src/data/Header.data.ts
+++ src/data/Header.data.ts
@@ -1,6 +1,47 @@
 export const HeaderData = [
-  "Home",
-  "About Us",
-  "What We Do",
-  "Get Involved",
+  {
+    label: "Home",
+    submenu: [
+      "Disease",
+      "Children Aid",
+      "Homelessness",
+      "Children & Youth",
+      "Environmental",
+    ],
+  },
+  {
+    label: "About Us",
+    submenu: [
+      "About Us",
+      "Our Pillars",
+      "Our History",
+      "Impact",
+      "The Future",
+      "Stories",
+    ],
+  },
+  {
+    label: "What We Do",
+    submenu: [
+      "What We Do",
+      "Empower Young People",
+      "Protecting Kids",
+      "Protecting Human Rights",
+      "Tackle Climate Change",
+      "Social Support",
+    ],
+  },
+  {
+    label: "Get Involved",
+    submenu: [
+      "Get Involved",
+      "Take Action",
+      "Volunteer",
+      "Host an Event",
+      "Participate in Events",
+      "Training Services",
+      "Join",
+      "Gifts in Kind",
+    ],
+  },
 ];
