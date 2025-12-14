9c7b2bd19dc9eeccbcae72b0a2eb2fc6431dfcc7
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-14 21:21:33 +0530

    Mission section updated style

--- src/components/Landing/Mission/Mission.tsx
+++ src/components/Landing/Mission/Mission.tsx
@@ -1,21 +1,37 @@
+import Image from "next/image";
+
 export default function Mission() {
   return (
-    <div className="items-center flex flex-col my-10 mx-10">
-      <div>OUR MISSION IS SIMPLE</div>
-      <div className="flex">
-        {/* "Floating" looking text */}
-        <div className="flex flex-col">
-          Stop deaths frpm breast cancer
-          <span className="cursor-pointer">
+    <section className="bg-primary mx-20 my-16">
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
+        <div className="max-w-2xl">
+          {/* "Floating" looking text */}
+          <p className="my-6 text-xl font-semibold tracking-wide text-gray-700">
+            OUR MISSION IS SIMPLE
+          </p>
+
+          <div className="text-9xl font-extrabold text-black">
+            Stop <span>deaths </span>
+            <span>from </span>
+            <span>breast </span>
+            <span>cancer</span>
+          </div>
+
+          <button className="my-4 text-sm font-bold text-black cursor-pointer">
             Learn more
-          </span>
+          </button>
         </div>
 
         {/* Image div */}
-        <div>
-          <img src="/Woman1.png"></img>
+        <div className="relative h-[600px] w-[600px]">
+          <Image
+            src="/Woman1.png"
+            alt=""
+            fill
+            className="object-cover"
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
