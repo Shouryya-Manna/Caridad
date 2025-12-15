57cd8b8435a85268f2d85dc679c00ed2a01b5ef7
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-15 20:06:53 +0530

    Data for Events Updated

---
+++ src/data/Events.data.ts
@@ -0,0 +1,23 @@
+export const events = [
+  {
+    date: "08",
+    month: "Mar",
+    title: "Big Pink Jump March",
+    time: "8:00 am",
+    location: "New York",
+  },
+  {
+    date: "16",
+    month: "Mar",
+    title: "Younger Women Together Online",
+    time: "18:00 pm",
+    location: "Online",
+  },
+  {
+    date: "21",
+    month: "Mar",
+    title: "Women V Cancer - Ride the Night",
+    time: "10:00 am",
+    location: "New York",
+  },
+];
