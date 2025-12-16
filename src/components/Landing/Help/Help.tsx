cf86641a28c94cde599715e5f1e993563691945a
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-16 09:04:55 +0530

    Help section updated

--- src/components/Landing/Help/Help.tsx
+++ src/components/Landing/Help/Help.tsx
@@ -14,12 +14,12 @@ export default function Help() {
       <div className="mx-auto flex flex-col max-w-7xl items-center justify-center gap-10 ">
         {/* Header */}
         <div className="mb-12 text-center">
-          <p className="mb-2 text-xs font-semibold tracking-widest text-muted-foreground">
+          <p className="mb-2 text-lg font-semibold tracking-widest">
             WHAT WE DO
           </p>
-          <h2 className="text-3xl font-bold md:text-4xl">
+          <p className="text-5xl font-bold md:text-4xl">
             How we help
-          </h2>
+          </p>
         </div>
 
         {/* Accordion */}
@@ -28,22 +28,21 @@ export default function Help() {
           collapsible
           className="w-full"
         >
-          <Separator />
           {HelpData.map((item, index) => (
             <AccordionItem
               key={index}
               value={`item+${index}`}
               className="border-0"
             >
-              <AccordionTrigger className="group flex items-center justify-between py-6 text-left text-lg font-semibold hover:no-underline">
+              <AccordionTrigger className="group cursor-pointer flex items-center justify-between py-10 text-left text-2xl font-bold tracking-tight hover:no-underline">
                 <span>{item.title}</span>
                 <span className="ml-4 flex h-8 w-8 items-center justify-center">
-                  <Plus className="h-5 w-5 transition-all group-data-[state=open]:hidden" />
-                  <X className="hidden h-5 w-5 transition-all group-data-[state=open]:block" />
+                  <Plus className="h-5 w-5 transition-all group-data-[state=open]:hidden text-gray-500" />
+                  <X className="hidden h-5 w-5 transition-all group-data-[state=open]:block text-gray-500" />
                 </span>
               </AccordionTrigger>
 
-              <AccordionContent className="pb-6 text-sm leading-relaxed text-muted-foreground">
+              <AccordionContent className="pb-10 pr-12 max-w-3xl text-base leading-relaxed text-muted-foreground">
                 {item.description}
               </AccordionContent>
 
