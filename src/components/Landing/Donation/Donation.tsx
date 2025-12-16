81baa477db768df640fe1b49fae654cbeb888de8
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-16 09:22:38 +0530

    Donation section done

--- src/components/Landing/Donation/Donation.tsx
+++ src/components/Landing/Donation/Donation.tsx
@@ -6,7 +6,7 @@ export default function Donation() {
   return (
     <section className="mx-auto max-w-7xl px-6 py-24">
       <div className="mb-16 text-center">
-        <p className="mb-4 text-sm font-semibold tracking-widest">
+        <p className="mb-4 text-lg font-semibold tracking-widest">
           GET INVOLVED
         </p>
         <h2 className="text-3xl font-bold md:text-4xl">
@@ -17,7 +17,8 @@ export default function Donation() {
       {/* Content */}
       <div className="grid grid-cols-1 overflow-hidden md:grid-cols-2 gap-10">
         {/* Image */}
-        <div className="relative h-[520px] md:h-[600px]">
+        {/* Image */}
+        <div className="relative h-[600px] md:h-[700px]">
           <Image
             src="/Woman3.png"
             alt=""
@@ -27,7 +28,7 @@ export default function Donation() {
         </div>
 
         {/* Actions */}
-        <div className="flex flex-col justify-center bg-amber-500 px-12 py-16">
+        <div className="flex flex-col justify-center bg-amber-500 px-12 py-16 space-y-10">
           {[
             "Donate",
             "Contribute",
@@ -35,10 +36,10 @@ export default function Donation() {
           ].map((item, index) => (
             <div key={item} className="group">
               <div className="flex cursor-pointer items-center justify-between py-10">
-                <h3 className="text-3xl font-semibold text-black">
+                <h3 className="text-5xl font-extrabold text-black">
                   {item}
                 </h3>
-                <ArrowRight className="h-7 w-17 text-black" />
+                <ArrowRight className="w-8 h-8 text-black transition-transform group-hover:translate-x-2" />
               </div>
               {index !== 2 && (
                 <Separator className="bg-black/30" />
