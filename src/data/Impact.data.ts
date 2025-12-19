04cdebab58246be80b71f7dddbbff16857dcce20
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-19 19:29:04 +0530

    Data Altered

--- src/data/Impact.data.ts
+++ src/data/Impact.data.ts
@@ -1,14 +1,14 @@
 export const ImpactData = [
   {
     imageUrl: "/Woman1.jpg",
-    data: "Breast cancer death rates declined 40% from 1989 to 2016 among women",
+    data: "There are more than 3.8 million breast cancer survivors in the United States",
   },
   {
     imageUrl: "/Woman2.jpg",
-    data: "Over the last decade Caridad has provided over 192,000 mammograms",
+    data: "Breast cancer death rates declined 40% from 1989 to 2016 among women",
   },
   {
     imageUrl: "/Woman3.jpg",
-    data: "There are more than 3.8 million breast cancer survivors in the United States",
+    data: "Over the last decade Caridad has provided over 192,000 mammograms",
   },
 ];
