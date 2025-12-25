a77970c5821255cfd519a0834a162001cc5929ce
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-25 16:58:28 +0530

    UI Fixes

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,5 @@
 import Charity from "@/components/AboutUs/Charity/Charity";
+import AFooter from "@/components/AboutUs/Footer/AFooter";
 import OurCauses from "@/components/AboutUs/OurCauses/OurCauses";
 import Outcome from "@/components/AboutUs/Outcome/Outcome";
 import Sponsor from "@/components/AboutUs/Sponsor/Sponsor";
@@ -19,6 +20,7 @@ export default function page() {
       <Sponsor />
       <Sponsor2 />
       <OurCauses />
+      <AFooter />
       <ScrollToTop />
     </div>
   );
