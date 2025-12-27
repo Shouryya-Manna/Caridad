437ef923ef87145f4e6b8949acfd12ecddb18545
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-27 14:22:41 +0530

    Sheet Ui fixed

--- src/components/Landing/Header/Components/SheetStyle.tsx
+++ src/components/Landing/Header/Components/SheetStyle.tsx
@@ -3,7 +3,7 @@ import {
   Sheet,
   SheetContent,
   SheetTrigger,
-} from "../../../ui/sheet";
+} from "@/components/ui/sheet";
 import { Bars2Icon } from "@heroicons/react/16/solid";
 
 export default function SheetStyle() {
@@ -13,7 +13,7 @@ export default function SheetStyle() {
         <SheetTrigger>
           <Bars2Icon className="h-8 w-8 cursor-pointer my-3" />
         </SheetTrigger>
-        <SheetContent className="px-7.5 w-full sm:max-w-[433px]">
+        <SheetContent className="px-7.5 w-full sm:max-w-[433px] overflow-y-auto">
           <div className="flex flex-col pl-5 pt-7 items-start justify-between">
             {[
               "Causes",
@@ -30,12 +30,12 @@ export default function SheetStyle() {
             ))}
 
             <div className="flex items-end w-full">
-              <Button className="cursor-pointer text-[16px] text-tertiary font-bold hover:bg-black hover:text-white rounded-4xl h-14 w-40 bg-[#FEC415]">
+              <Button className="cursor-pointer mt-8 text-[16px] text-tertiary font-bold hover:bg-black hover:text-white rounded-4xl h-15 w-42 bg-[#FEC415]">
                 Donate
               </Button>
             </div>
             <div>
-              <div className="grid grid-cols-3 gap-4">
+              <div className="grid grid-cols-3 gap-8 mt-10">
                 {/* facebook */}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +61,7 @@ export default function SheetStyle() {
                   <path d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
                 </svg>
               </div>
-              <div className="flex items-center justify-between py-2">
+              <div className="flex items-center justify-between py-7.5 gap-x-5 text-sm">
                 <span>Terms of Use</span>
                 <span>Media Kit</span>
                 <span>Contacts</span>
