65c42c082910a1ba4d6ec894af797673a5823f41
SushritaBiswas <sushritabiswas19@gmail.com>
2025-12-30 16:19:21 +0530

    Sponsor Child Modularity Fixed

--- src/components/GetInvolved/SponsorChild.tsx
+++ src/components/GetInvolved/SponsorChild.tsx
@@ -1,11 +1,14 @@
+"use client";
 import { Smile } from "lucide-react";
-import Image from "next/image";
 import UnderlinedText from "../Global/UnderlinedText";
+import Card from "../Global/Card/Card";
+import { SponsorChildData } from "@/data/SponsorChild.data";
+import { cn } from "@/lib/utils";
 
 export default function SponsorChild() {
   return (
     <section className="max-w-8xl mx-auto w-full grid grid-cols-3 px-7.5 py-30 bg-[#F8F9F3]">
-      <div className="flex flex-col items-start">
+      {/* <div className="flex flex-col items-start">
         <div className="flex flex-col items-start">
           <div className="relative h-[355px] w-[275px] mb-6">
             <Image
@@ -65,7 +68,33 @@ export default function SponsorChild() {
         <button className="text-[16px] text-tertiary font-bold cursor-pointer hover:bg-black hover:text-white transition-colors duration-300 rounded-4xl h-16.5 w-45 bg-[#FEC415]">
           Sponsor
         </button>
-      </div>
+      </div> */}
+
+      {SponsorChildData.map((card, index) => (
+        <div
+          key={index}
+          className={cn(
+            "flex flex-col items-start",
+            index === 1 && "justify-center mt-30"
+          )}
+        >
+          <Card card={card}>
+            <div className="relative h-[380px] w-[280px]">
+              <Card.Image />
+            </div>
+            <Card.Title />
+            <div className="flex items-center gap-3.5 text-base text-tertiary my-3.5">
+              <Smile className="h-5 w-5" />
+              <Card.Designation />
+            </div>
+            <Card.Paragraph className="my-1 max-w-[280px] leading-loose" />
+
+            <button className="mt-5 text-base text-tertiary font-bold cursor-pointer hover:bg-black hover:text-white transition-colors duration-300 rounded-4xl h-16.5 w-45 bg-[#FEC415]">
+              Sponsor
+            </button>
+          </Card>
+        </div>
+      ))}
 
       <div className="flex flex-col justify-center ">
         <p className="mb-3 text-[20px] font-[401] uppercase">
---
+++ src/data/SponsorChild.data.ts
@@ -0,0 +1,16 @@
+export const SponsorChildData = [
+  {
+    image: "/About-Us/Ab7.jpg",
+    title: "Isaiah",
+    designation: "12 Years",
+    paragraph:
+      "Hello, my name is Isaiah and I am 12 years old and I want to be a doctor when I grow up.",
+  },
+  {
+    image: "/About-Us/Ab8.jpg",
+    title: "Samantha",
+    designation: "6 Years",
+    paragraph:
+      "Hello, my name is Samantha and I am 6 years old and I want to be a ballet dancer when I grow up.",
+  },
+];
